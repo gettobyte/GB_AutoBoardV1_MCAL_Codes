d32a84e51cfdb75f3664a2eb6c79951fca76bc69
rohans002 <singhal.rohan002@gmail.com>
2025-05-28 16:48:31 +0530

    PIT implemented and running

--- src/main.c
+++ src/main.c
@@ -30,6 +30,12 @@
 #define BCTU_USED_FIFO_IDX 0U
 #define BCTU_FIFO_WATERMARK 1U
 #define ADC_TOLERANCE(x,y) (((x > y) ? (x - y) : (y - x)) > 200U) /* Check that the data is within tolerated range */
+/* PIT instance used - 0 */
+#define PIT_INST_0 0U
+/* PIT Channel used - 0 */
+#define CH_0 0U
+/* PIT time-out period - equivalent to 1s */
+#define PIT_PERIOD 80000000
 
 volatile int exit_code = 0;
 volatile boolean notif_triggered = FALSE;
@@ -59,7 +65,7 @@ void AdcEndOfChainNotif(void)
 
 void BctuWatermarkNotif(void)
 {
-    uint8 idx;
+//    uint8 idx;
     notif_triggered = TRUE;
 	/* Checks the measured ADC data conversion */
 	while (data < 3500UL)
@@ -78,6 +84,7 @@ int main(void)
 
     /* Initialize and configure drivers */
     clockStatus = Clock_Ip_Init(&Clock_Ip_aClockConfig[0]);
+
     while (clockStatus != CLOCK_IP_SUCCESS)
     {
         clockStatus = Clock_Ip_Init(&Clock_Ip_aClockConfig[0]);
@@ -86,24 +93,21 @@ int main(void)
     Bctu_Ip_Init(BCTUHWUNIT_0_VS_0_INSTANCE, &BctuHwUnit_0_VS_0);
 
     status = (StatusType) Adc_Sar_Ip_Init(ADCHWUNIT_0_VS_0_INSTANCE, &AdcHwUnit_0_VS_0);
+
     while (status != E_OK);
 
     /* Install and enable interrupt handlers */
     IntCtrl_Ip_InstallHandler(ADC0_IRQn, Adc_Sar_0_Isr, NULL_PTR);
+
     IntCtrl_Ip_InstallHandler(BCTU_IRQn, Bctu_0_Isr, NULL_PTR);
-//    IntCtrl_Ip_InstallHandler(PIT0_IRQn, PIT_0_ISR, NULL_PTR);
+
+    IntCtrl_Ip_InstallHandler(PIT0_IRQn, PIT_0_ISR, NULL_PTR);
 
     IntCtrl_Ip_EnableIrq(ADC0_IRQn);
-    IntCtrl_Ip_EnableIrq(BCTU_IRQn);
-//    IntCtrl_Ip_EnableIrq(PIT0_IRQn);
 
-//    Pit_Ip_Init(PIT_0_IP_INSTANCE_NUMBER, &PIT_0_InitConfig_PB);
-//
-//    Pit_Ip_InitChannel(PIT_0_IP_INSTANCE_NUMBER, PIT_0_ChannelConfig_PB);
-//
-//    Pit_Ip_EnableChannelInterrupt(PIT_0_IP_INSTANCE_NUMBER, PIT_0_CH_0);
-//
-//    Pit_Ip_StartChannel(PIT_0_IP_INSTANCE_NUMBER, PIT_0_CH_0PIT_0_CH_0, countValue);
+    IntCtrl_Ip_EnableIrq(BCTU_IRQn);
+    IntCtrl_Ip_EnableIrq(PIT0_IRQn);
+
 
     /* Call Calibration function multiple times, to mitigate instability of board source */
     for(Index = 0; Index <= 5; Index++)
@@ -117,32 +121,40 @@ int main(void)
 
     Adc_Sar_Ip_EnableNotifications(ADCHWUNIT_0_VS_0_INSTANCE, ADC_SAR_IP_NOTIF_FLAG_NORMAL_ENDCHAIN | ADC_SAR_IP_NOTIF_FLAG_INJECTED_ENDCHAIN);
 
+    Pit_Ip_Init(PIT_INST_0, &PIT_0_InitConfig_PB);
+
+    Pit_Ip_InitChannel(PIT_INST_0, PIT_0_CH_0);
+
+    Pit_Ip_EnableChannelInterrupt(PIT_INST_0, CH_0);
+
+    Pit_Ip_StartChannel(PIT_INST_0, CH_0, PIT_PERIOD);
+
     /* Start a SW triggered normal conversion on ADC_SAR */
-    Adc_Sar_Ip_StartConversion(ADCHWUNIT_0_VS_0_INSTANCE, ADC_SAR_IP_CONV_CHAIN_NORMAL);
+//    Adc_Sar_Ip_StartConversion(ADCHWUNIT_0_VS_0_INSTANCE, ADC_SAR_IP_CONV_CHAIN_NORMAL);
 
     /* Wait for the notification to be triggered and read the data */
 //    while (notif_triggered != TRUE);
-    notif_triggered = FALSE;
+//    notif_triggered = FALSE;
 
     /* Start a SW triggered injected conversion on ADC_SAR */
-    Adc_Sar_Ip_StartConversion(ADCHWUNIT_0_VS_0_INSTANCE, ADC_SAR_IP_CONV_CHAIN_INJECTED);
+//    Adc_Sar_Ip_StartConversion(ADCHWUNIT_0_VS_0_INSTANCE, ADC_SAR_IP_CONV_CHAIN_INJECTED);
 
     /* Wait for the notification to be triggered and read the data */
 //    while (notif_triggered != TRUE);
-    notif_triggered = FALSE;
+//    notif_triggered = FALSE;
 
     /* Start a SW triggered conversion on BCTU using a single trigger */
-    Bctu_Ip_SetGlobalTriggerEn(BCTUHWUNIT_0_VS_0_INSTANCE, TRUE);
-    Bctu_Ip_EnableNotifications(BCTUHWUNIT_0_VS_0_INSTANCE, BCTU_IP_NOTIF_FIFO1);
-    Bctu_Ip_SwTriggerConversion(BCTUHWUNIT_0_VS_0_INSTANCE, BCTU_USED_SINGLE_TRIG_IDX);
+//    Bctu_Ip_SetGlobalTriggerEn(BCTUHWUNIT_0_VS_0_INSTANCE, TRUE);
+//    Bctu_Ip_EnableNotifications(BCTUHWUNIT_0_VS_0_INSTANCE, BCTU_IP_NOTIF_FIFO1);
+//    Bctu_Ip_SwTriggerConversion(BCTUHWUNIT_0_VS_0_INSTANCE, BCTU_USED_SINGLE_TRIG_IDX);
 
     /* Wait for the watermark notification to be triggered and then read the data */
-    while (notif_triggered != TRUE);
+//    while (notif_triggered != TRUE);
 
     /* Deinitialize drivers */
-    Bctu_Ip_Deinit(BCTUHWUNIT_0_VS_0_INSTANCE);
-    status = (StatusType) Adc_Sar_Ip_Deinit(ADCHWUNIT_0_VS_0_INSTANCE);
-    while (status != E_OK);
+//    Bctu_Ip_Deinit(BCTUHWUNIT_0_VS_0_INSTANCE);
+//    status = (StatusType) Adc_Sar_Ip_Deinit(ADCHWUNIT_0_VS_0_INSTANCE);
+//    while (status != E_OK);
 
     for(;;)
     {
